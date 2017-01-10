0b1dcd6fbf458cf9e2fa9971655f7b2fcce8d8c2
Mike Waterworth <mike.waterworth@epimorphics.com>
2017-01-10 10:03:24 +0000

    Use express instead of HTTP

--- index.js
+++ index.js
@@ -2,31 +2,34 @@
 import transform from './transform' // Transform function
 import superAgent from 'superagent' // Load data from the API
 import config from './config' // Load data from the API
+var express = require('express')
 
-const http = require('http')
 const port = process.env.PORT ? process.env.PORT : 3000
+var app = express()
 
-const requestHandler = (request, response) => {
-  if(request.url === '/'){
-    superAgent
-      .get(config.baseUrl)
-      .end(function(err, res) {
-        if (err) {
-          return response.end('' + err)
-        }
-        let geoJSON = transform.transformToGeoJSON(res.body.items)
-        response.end(JSON.stringify(geoJSON))
-      });
-  } else {
-    response.end('use /')
-  }
-}
+app.get('/api/', function (req, res) {
+  console.log(req.query)
+  superAgent
+    .get(req.query.endpoint)
+    .end(function(err, apiRes) {
+      if (err) {
+        res.status(500)
+        return res.end('' + err)
+      }
+      try {
+        let geoJSON = transform.transformToGeoJSON(apiRes.body.items)
+        res.end(JSON.stringify(geoJSON))
+      } catch (e) {
+        res.status(500)
+        return res.end(`Please ensure you're connecting to a JSON API and that it returns objects with 'lat' and 'long' properties`)
+      }
+    });
+})
 
-const server = http.createServer(requestHandler)
+app.get('/', function (req, res) {
+  res.sendFile('index.html' , { root : __dirname})
+})
 
-server.listen(port, (err) => {
-  if (err) {
-    return console.log('something bad happened', err)
-  }
-  console.log(`server is listening on ${port}`)
+app.listen(port, function () {
+  console.log(`API proxy listening on port ${port}`)
 })
